7c860100c59f934648e181ca47433bb3d0f52c90
shuuji3 <shuuji3@gmail.com>
2017-01-20 18:17:12 +0900

    add PATH for gcloud & hub command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,3 +20,11 @@ status --is-interactive; and source (scalaenv init -|psub)
 # go
 set -g fish_user_paths $fish_user_paths /usr/local/opt/go/libexec/bin
 
+# google-cloud-sdk
+set -x PATH /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin $PATH
+set -x MANPATH /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/help/man /usr/local/share/man /usr/share/man /opt/x11/share/man
+
+# hub
+function git --description 'Alias for hub, which wraps git to provide extra functionality with GitHub.'
+    hub $argv
+end
