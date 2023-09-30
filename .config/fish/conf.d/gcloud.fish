57016a1e9b72a243dea49f49a5deb09fec38011f
TAKAHASHI Shuuji <shuuji3@gmail.com>
2023-10-01 01:30:46 +0900

    feat(fish): increase cloud upload bandwidth

--- .config/fish/conf.d/gcloud.fish
+++ .config/fish/conf.d/gcloud.fish
@@ -1,5 +1,9 @@
 . (brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
 
+# Increase upload bandwidth
+# ref. https://cloud.google.com/iap/docs/using-tcp-forwarding
+export CLOUDSDK_PYTHON_SITEPACKAGES=1
+
 # Select configurations with fzf
 alias gcloud-config="CLOUDSDK_ACTIVE_CONFIG_NAME=(FZF_DEFAULT_COMMAND=\"gcloud config configurations list --format='get(name)'\" fzf --ansi --no-preview)"
 
