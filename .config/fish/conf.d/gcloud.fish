166704da76679155419941925914d16ff2bfba27
TAKAHASHI Shuuji <shuuji3@gmail.com>
2023-10-01 01:29:49 +0900

    feat(fish): add gcloud-config alias

--- .config/fish/conf.d/gcloud.fish
+++ .config/fish/conf.d/gcloud.fish
@@ -1,2 +1,5 @@
 . (brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
 
+# Select configurations with fzf
+alias gcloud-config="CLOUDSDK_ACTIVE_CONFIG_NAME=(FZF_DEFAULT_COMMAND=\"gcloud config configurations list --format='get(name)'\" fzf --ansi --no-preview)"
+
